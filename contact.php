1c2112356c2bd10508e6484059c993406637dad2
eeldiep91 <111565743+eeldiep91@users.noreply.github.com>
2023-05-26 17:05:54 +0700

    add contact us

--- contact.php
+++ contact.php
@@ -1,6 +1,5 @@
 <?php include('layout/header.php') ?>
-
-
+<?php include('backend/submit-contact.php') ?>
 
 <!-- Page Add Section Begin -->
 <section class="page-add">
@@ -11,9 +10,9 @@
                     <h2>Contact us<span>.</span></h2>
                 </div>
             </div>
-            <?php
-            include('layout/discount.php');
-            ?>
+            <div class="col-lg-8">
+                <img src="img/add.jpg" alt="">
+            </div>
         </div>
     </div>
 </section>
@@ -24,24 +23,23 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-8">
-                <form action="#" class="contact-form">
-                    <div class="row">
-                        <div class="col-lg-6">
-                            <input type="text" placeholder="First Name">
+            <?php if(!empty($statusMsg)){ ?>
+                <div class="status-msg <?php echo $status; ?>"><?php echo $statusMsg; ?> </div>
+                <?php } ?>
+                    <form action="" method="POSt" class="contact-form">
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <input type="text" placeholder="Name" name="name">
+                                <input type="email" placeholder="E-mail" name="email">
+                                <input type="text" placeholder="Subject" name="subject">
+                                <textarea placeholder="Message" name="message"></textarea>
+                            </div>
+                            
+                            <div class="col-lg-12 text-right">
+                                <button type="submit" name="submit" value="Submit">Send message</button>
+                            </div>
                         </div>
-                        <div class="col-lg-6">
-                            <input type="text" placeholder="Last Name">
-                        </div>
-                        <div class="col-lg-12">
-                            <input type="email" placeholder="E-mail">
-                            <input type="text" placeholder="Subject">
-                            <textarea placeholder="Message"></textarea>
-                        </div>
-                        <div class="col-lg-12 text-right">
-                            <button type="submit">Send message</button>
-                        </div>
-                    </div>
-                </form>
+                    </form>
             </div>
             <div class="col-lg-3 offset-lg-1">
                 <div class="contact-widget">
